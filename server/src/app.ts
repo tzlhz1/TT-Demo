3cca05e832a6641eed1397fdd162bb11795f5c14
tzlhz1 <wx_b867bd31502a4596bb1e99b313c41b9f@git.code.tencent.com>
2020-12-21 20:25:27 +0800

    fix:尝试构建ts

---
+++ server/src/app.ts
@@ -0,0 +1,9 @@
+// import * as http from 'http';
+
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-type':'text/plain'});
+//     res.end('hello')
+// }).listen(8888)
+// console.log('启动')
+
+var a:string = '123'
