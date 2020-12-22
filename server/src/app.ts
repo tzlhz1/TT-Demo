fed283e44ed77368b42b79fd3a0cbc1dfa1c1c94
tzlhz1 <wx_b867bd31502a4596bb1e99b313c41b9f@git.code.tencent.com>
2020-12-22 10:58:35 +0800

    fix:修改ts node 更新问题

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,7 @@
-// import * as http from 'http';
+import * as http from 'http';
 
-// http.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-type':'text/plain'});
-//     res.end('hello')
-// }).listen(8888)
-// console.log('启动')
-
-var a:string = '123'
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'text/plain'});
+    res.end('hello')
+}).listen(8888)
+console.log('启动1')
